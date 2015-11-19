3789cc50ce89bcef1c7b2bb051e6a6aa66838c1a
Matheus Leonardo <mlsm@trialent.com>
2015-11-19 17:20:16 -0300

    feat: add TRIAL developer footer component
    Introduces a dedicated footer section for TRIAL developer pages with branding logo and copyright information.
    
    This footer component provides consistent branding and legal compliance for TRIAL developer-facing content.

---
+++ html/footer/TRIAL/developer.php
@@ -0,0 +1,4 @@
+<section id="footer-developer">
+            <div id="logo-developer" style="background-image: url(/no-redo/images/TRIAL_branding.png); width: 50px; height: 50px"></div>
+            <span id="copyright">2015 ₢ TRIAL. Todos os direitos reservados.</span>
+        </section>
