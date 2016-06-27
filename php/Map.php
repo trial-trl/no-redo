2d2ad738701bb3ee333c511f3c8178918af3100a
Matheus Leonardo <mlsm@trialent.com>
2016-06-27 16:26:24 -0300

    feat: add StreetRaceAccount class and improve cookie management
    - Add StreetRaceAccount class with authentication and profile management
    - Refactor logout to dynamically clear all TRL_ prefixed cookies with domain detection
    - Update makePermanentLogin to use arrays and dynamic domain selection
    - Add Map API endpoints (gstats, gcntes) for mobile and web
    - Fix Map.php include paths and request handling
    - Add request() utility function for HTTP requests
    - Add Street Race cookie constants (COOKIE_STREET_RACE_ID, COOKIE_STREET_RACE_TYPE)
    - Rename NoRedo cookie constants to use TRL_ prefix
    - Add URL_ACCOUNT constant
    - Remove unused context constants from TRIALAccount
    - Update database credentials for production

--- php/Map.php
+++ php/Map.php
@@ -4,12 +4,14 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ * 
+ * Last edition: 18/06/2016
  */
 
-include_once $_SERVER['DOCUMENT_ROOT'] . '/no-redo/repository/php/utils/Constant.php';
-include_once $_SERVER['DOCUMENT_ROOT'] . '/no-redo/repository/php/utils/Utils.php';
-include_once $_SERVER['DOCUMENT_ROOT'] . '/no-redo/repository/php/ConnectDB.php';
-include_once $_SERVER['DOCUMENT_ROOT'] . '/no-redo/repository/php/Map.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/repository/php/ConnectDB.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/repository/php/utils/Constant.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/repository/php/utils/Utils.php';
+
 date_default_timezone_set('America/Sao_Paulo');
 $request = json_decode(file_get_contents('php://input'));
 
@@ -40,23 +42,32 @@ if ($request != null) {
     $r = $request->{'r'};
     $requestType = 'mobile';
 } else {
-    if (filter_input(INPUT_SERVER, 'REQUEST_METHOD') == 'GET') {
+    if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 	$r = filter_input(INPUT_GET, 'r');
     } else {
 	$r = filter_input(INPUT_POST, 'r');
     }
     $requestType = 'web';
+    $REQUEST = $_SERVER['REQUEST_METHOD'] == 'GET' ? INPUT_GET : INPUT_POST;
 }
 
 $instance_class = new Map();
-    
 if ($requestType === 'mobile') {
     switch ($r) {
+        case 'gstats':
+            echo json_encode($instance_class->getStates());
+            break;
         case 'gcntes':
             echo json_encode($instance_class->getCounties($request->{'state'}));
             break;
     }
 } else if ($requestType === 'web') {
     switch ($r) {
+        case 'gstats':
+            echo json_encode($instance_class->getStates());
+            break;
+        case 'gcntes':
+            echo json_encode($instance_class->getCounties(filter_input($REQUEST, 'state')));
+            break;
     }
 }
