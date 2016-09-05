20b7c4d197b68438bb9d616f131dc2cb68b555ba
Matheus Leonardo <mlsm@trialent.com>
2016-09-04 23:01:46 -0300

    feat: add SQLUtils query builder package
    - Add Query abstract base class with parameter binding utilities
    - Add Select query builder with JOIN, WHERE, ORDER BY, and GROUP BY support
    - Add Insert query builder with column and value binding
    - Add SelectClauses and InsertClauses interfaces
    - Add table display CSS classes (trl-table, trl-trow, trl-tcolumn)
    - Add DATABASE_FIX_IT constant

---
+++ php/SQLUtils/InsertClauses.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Description of InsertClauses
+ *
+ * Created on 04/09/2016, ~22:34:13
+ * @author Matheus Leonardo dos Santos Martins
+ * @copyright (c) 2016, TRIAL
+ * 
+ * @package SQLUtils
+ */
+interface InsertClauses {
+    
+    
+    
+}
