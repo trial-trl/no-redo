81f0f79d296dabfb377e42e730f2bd692d779b53
Matheus Leonardo <mlsm@trialent.com>
2016-09-10 23:04:02 -0300

    refactor: introduce Message class and improve SQLUtils
    - Add Message class to replace MESSAGE_* constants
    - Update TRIALAccount to use Message class constants throughout
    - Add account type constants to TRIALAccount (USER, INSTITUTION, INSTITUTION_MEMBER, GOVERNMENT)
    - Fix Institution jsonSerialize() to return proper data structure
    - Add encoded property to DecodeInstitutionInfos and make InstitutionActivity implement JsonSerializable
    - Add return type hints to SQLUtils query methods (: QueryResponse, : array)
    - Add limit() method to Select query builder
    - Fix Update class name (was incorrectly named Delete)
    - Add Files.php for centralized includes
    - Always include 'total' in Select query responses
    - Remove incorrect require_once path from TRIALAccount

---
+++ php/utils/Message.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Description of Message
+ * 
+ * Created on 10/09/2016, ~15:40:07
+ * @author Matheus Leonardo dos Santos Martins
+ * @copyright (c) 2016, TRIAL
+ * 
+ * @package Utils
+ */
+
+class Message {
+    
+    const EXIST = 'E';
+    const NOT_EXIST = 'NE';
+    const NOT_ACTIVATED = "NA";
+    const SAVED_WITH_SUCCESS = 'SWS';
+    const ERROR_PASSWORD_INCORRECT = 'ERR_PI';
+    const ERROR = 'ERR';
+    const MEMBER_WITHOUT_INSTITUTION = "MWI";
+    
+}
