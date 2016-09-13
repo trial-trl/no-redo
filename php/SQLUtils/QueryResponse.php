3fbfde24a79b26c661ca9cf9b5de84cc3de5456f
Matheus Leonardo <mlsm@trialent.com>
2016-09-12 22:43:01 -0300

    refactor: introduce base Response and Error classes and add Request utility
    - Add Response base class with success, result, and error handling
    - Add Error base class with code and message properties
    - Refactor QueryResponse to extend Response base class
    - Move QueryError to separate file and extend Error base class
    - Add Request class for fluent HTTP requests using cURL
    - Add RequestResponse class extending Response for HTTP responses
    - Fix RequestResponse to use curl_errno with undefined $ch variable

--- php/SQLUtils/QueryResponse.php
+++ php/SQLUtils/QueryResponse.php
@@ -8,16 +8,17 @@
  * 
  * @package SQLUtils
  */
-class QueryResponse {
+
+require_once '../utils/Response.php';
+require_once 'QueryError.php';
+
+class QueryResponse extends Response {
     
     private $statement;
-    private $success;
-    private $query_error;
-    private $result;
     
     public function __construct(&$statement, &$bind, $success) {
         $this->statement = $statement;
-        $this->success = $this->statement->execute($bind);
+        parent::__construct($this->statement->execute($bind));
         if ($this->success()) {
             if ($success) {
                 $this->setResult($success());
@@ -29,56 +30,6 @@ class QueryResponse {
     
     public function existRows() {
         return $this->success() ? $this->statement->rowCount() > 0 : false;
-    }
-    
-    public function success() {
-        return $this->success;
-    }
-    
-    private function setResult($result) {
-        $this->result = $result;
-    }
-    
-    public function getResult() {
-        return $this->result;
-    }
-    
-    private function setError(QueryError $error) {
-        $this->query_error = $error;
-    }
-    
-    public function getError() {
-        return $this->query_error;
-    }
-
-}
-
-class QueryError implements JsonSerializable {
-    
-    private $code;
-    private $sqlstate;
-    private $message;
-    
-    public function __construct(array $error_info) {
-        $this->code = $error_info[0];
-        $this->sqlstate = $error_info[1];
-        $this->message = $error_info[2];
-    }
-    
-    public function getCode() {
-        return $this->code;
-    }
-    
-    public function getSQLState() {
-        return $this->sqlstate;
-    }
-    
-    public function getMessage() {
-        return $this->message;
-    }
-
-    public function jsonSerialize() {
-        return ['code' => $this->getCode(), 'sqlstate' => $this->getSQLState(), 'message' => $this->getMessage];
     }
 
 }
