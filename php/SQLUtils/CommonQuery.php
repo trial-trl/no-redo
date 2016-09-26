00934a6a779c81ecb6842de120710ef1d5d7c70b
Matheus Leonardo <mlsm@trialent.com>
2016-09-26 00:04:51 -0300

    feat: add SQLExec class and returnFoundRows support
    - Add SQLExec class for executing raw SQL queries with QueryResponse
    - Add CommonQuery abstract base class for shared connection and statement properties
    - Refactor Query class to extend CommonQuery
    - Add returnFoundRows() method to Select query builder for SQL_CALC_FOUND_ROWS support
    - Simplify Delete prepare() method by removing unnecessary binding logic
    - Add SQLExec to Files.php includes

---
+++ php/SQLUtils/CommonQuery.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Description of CommonQuery
+ *
+ * Created on 24/09/2016, 23:57:39
+ * @author Matheus Leonardo dos Santos Martins
+ * @copyright (c) 2016, TRIAL
+ * 
+ * @package SQLUtils
+ */
+
+require_once 'QueryResponse.php';
+
+abstract class CommonQuery {
+    
+    public $conn;
+    public $statement;
+    
+    /**
+     * 
+     * 
+     * @param PDO $conn
+     */
+    public function __construct(PDO $conn) {
+        $this->conn = $conn;
+    }
+    
+    /**
+     * 
+     * 
+     * @param PDO $conn
+     */
+    public function conn(PDO $conn) {
+        $this->conn = $conn;
+        return $this;
+    }
+    
+}
