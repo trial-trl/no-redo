2e3acfd46c86731cf8eca8345c94ea10005df026
Matheus Leonardo <mlsm@trialent.com>
2016-10-05 16:15:16 -0300

    fix: update getAllAccounts to use QueryResponse pattern and add FETCH_ASSOC to SQLExec
    - Update getAllAccounts() to use QueryResponse pattern with success check
    - Add PDO::FETCH_ASSOC to SQLExec::run() for consistent associative array results

--- php/SQLUtils/SQLExec.php
+++ php/SQLUtils/SQLExec.php
@@ -13,6 +13,13 @@ require_once 'CommonQuery.php';
 
 class SQLExec extends CommonQuery {
     
+    /**
+     * 14/10/2016, 02:35:46:
+     *      added FETCH_ASSOC option
+     * 
+     * @param string $exec
+     * @return \QueryResponse
+     */
     public function run(string $exec) {
         $this->statement = $this->conn->prepare($exec);
         return new QueryResponse($this->statement, null, function () {
@@ -20,7 +27,7 @@ class SQLExec extends CommonQuery {
             $count_rows = $this->statement->rowCount();
             if ($count_rows) {
                 if (strpos($this->statement->queryString, 'INSERT') === false && strpos($this->statement->queryString, 'UPDATE') === false) {
-                    $response = $this->statement->fetchAll();
+                    $response = $this->statement->fetchAll(PDO::FETCH_ASSOC);
                 } else {
                     $response['id'] = $this->conn->lastInsertId();
                 }
