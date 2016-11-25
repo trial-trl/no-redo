d9a0d1f1a9bff13e7182021b1abf094d0049d8aa
Matheus Leonardo <mlsm@trialent.com>
2016-11-24 23:56:18 -0300

    refactor: introduce Account base class and consolidate profile classes
    - Add Account base class with common properties and methods (id, email, password, activated, checkPassword, isActivated, getPhotoUrl)
    - Add Government class extending Account for government accounts
    - Refactor User and Institution to extend Account and remove duplicate methods
    - Update FixItAccount to use DATABASE_FIX_IT and Query::helper pattern
    - Replace buildResponse() with Query::helper() throughout TRIALAccount
    - Add account type to authentication response objects
    - Fix governmentAuth to use Government class instead of Institution
    - Fix concludeAuthenticationWeb instanceof check for Government
    - Fix FixItAccount login
    - Add support for government accounts in FixItAccount authenticateUser()

--- php/Accounts/FixItAccount.php
+++ php/Accounts/FixItAccount.php
@@ -8,6 +8,7 @@
  * @author Matheus Leonardo dos Santos Martins
  * @copyright (c) 2016, TRIAL
  * 
+ * @version 1.01NC
  * @package Account
  */
 
@@ -16,32 +17,29 @@ class FixItAccount {
     private $con;
     
     public function __construct() {
-        $this->con = (new ConnectDB(DB_PREFIX . DATABASE_STREET_RACE))->connect();
+        $this->con = (new ConnectDB(DB_PREFIX . DATABASE_FIX_IT))->connect();
     }
     
-    public function authenticateUser($id_trial) {
-        $profile = $this->getProfile($id_trial);
-        if ($profile['message'] === MESSAGE_EXIST) {
-            $domain = filter_input(INPUT_SERVER, 'HTTP_HOST') !== 'localhost' ? '.trialent.com' : 'localhost';
-            setcookie(COOKIE_FIX_IT_ID, $profile['id'], time() + (60 * 60 * 24 * 365), '/', $domain);
+    public function authenticateUser($id_trial, string $type = TRIALAccount::USER) {
+        $profile = $this->getProfile($id_trial, $type);
+        if ($profile['message'] === Message::NOT_EXIST) {
+            $profile = ['level' => 1, 'experience' => 0, 'id' => Query::helper((new Insert($this->con))->table($type === TRIALAccount::USER ? 'users' : 'governments')->columns('user, register_date_time')->values([$id_trial, date('Y-m-d H:i:s')])->run(), function ($query) {
+                return $query->getResult()['id'];
+            })];
         }
+        $domain = filter_input(INPUT_SERVER, 'HTTP_HOST') !== 'localhost' ? '.trialent.com' : 'localhost';
+        setcookie(COOKIE_FIX_IT_ID, $profile['id'], time() + (60 * 60 * 24 * 365), '/', $domain);
         return $profile;
     }
     
-    public function getProfile($user) {
-        $result = selectDB($this->con, 'users', 'id, user', 'WHERE user = :user', [':user' => $user]);
-        if ($result != null) {
-            $result = $result[0];
-        } else {
-            $result = ['user' => $user, 'type' => 'STUDENT', 'id' => insertDB($this->con, 'users', 'user, type, can_use_app, register_date, register_time', [$result['user'], $result['type'], 0, date('Y-m-d'), date('H:i:s')])['id']];
-        }
-        $result['message'] = $result != null ? MESSAGE_EXIST : MESSAGE_NOT_EXIST;
-        return $result;
-    }
-    
-    public function canUseApp($user) {
-        $profile = $this->getProfile($user);
-        return $profile['message'] === MESSAGE_EXIST && $profile['can_use_app'];
+    public function getProfile($user, string $type = TRIALAccount::USER) {
+        return Query::helper((new Select($this->con))->table($type === TRIALAccount::USER ? 'users' : 'governments')->columns($type === TRIALAccount::USER ? 'id, user, level, experience' : 'id, user')->where('user = :user')->values([':user' => $user])->run(), function ($query) {
+            if ($query->existRows()) {
+                $result = $query->getResult()[0];
+            }
+            $result['message'] = $query->existRows() ? Message::EXIST : Message::NOT_EXIST;
+            return $result;
+        });
     }
     
 }
