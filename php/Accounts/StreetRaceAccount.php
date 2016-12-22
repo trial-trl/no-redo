8760770d42995bb66e8a7bed55cd1cfc3d321947
Matheus Leonardo <mlsm@trialent.com>
2016-12-21 23:38:49 -0300

    refactor: modernize StreetRaceAccount to use query builder pattern
    - Convert all methods to static and remove instance constructor
    - Replace ConnectDB with DB::connect() and use Query::helper() pattern
    - Update createAccount() to use Insert builder and accept only user parameter
    - Update getProfile() to use Select builder with Query::helper() pattern
    - Replace MESSAGE_* constants with Message class constants
    - Consolidate register_date and register_time into register_date_time column
    - Add PHPDoc header documentation

--- php/Accounts/StreetRaceAccount.php
+++ php/Accounts/StreetRaceAccount.php
@@ -1,22 +1,27 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Description of TRIALAccount
+ *
+ * Created on 27/06/2016, 19:23:38
+ * @author Matheus Leonardo dos Santos Martins
+ * @copyright (c) 2016, TRIAL
+ * 
+ * @package Accounts
+ * 
+ * @version 1.0NC
  */
+
+/*
+ * 21/12/2016, 22:46:48 - 23:38:26 => update all code to Newer Codes, and all function are static now
+ */
+
+use SQL\Query, SQL\Select, SQL\Insert;
 
 class StreetRaceAccount {
     
-    private $con;
-    
-    public function __construct() {
-        $this->con = (new ConnectDB(DB_PREFIX . DATABASE_STREET_RACE))->connect();
-    }
-    
-    public function authenticateUser($id_trial) {
-        $profile = $this->getProfile($id_trial);
-        if ($profile['message'] === MESSAGE_EXIST) {
+    public static function authenticateUser($id_trial) {
+        $profile = self::getProfile($id_trial);
+        if ($profile['message'] === Message::EXIST) {
             $domain = filter_input(INPUT_SERVER, 'HTTP_HOST') !== 'localhost' ? '.trialent.com' : 'localhost';
             setcookie(COOKIE_STREET_RACE_ID, $profile['id'], time() + (60 * 60 * 24 * 365), '/', $domain);
             setcookie(COOKIE_STREET_RACE_TYPE, $profile['type'], time() + (60 * 60 * 24 * 365), '/', $domain);
@@ -24,28 +29,25 @@ class StreetRaceAccount {
         return $profile;
     }
     
-    public function createAccount($user, $institution, $type) {
-        $result = insertDB($this->con, 'users', 'user, institution, type, register_date, register_time, ip', array($user, $institution, $type, date('Y-m-d'), date('H:i:s'), getenv('REMOTE_ADDR')));
-        $result['message'] = MESSAGE_SAVED_WITH_SUCCESS;
-        return $result;
+    public static function createAccount($user) {
+        return Query::helper((new Insert(DB::connect(DATABASE_STREET_RACE)))->table('users')->columns('user, type, can_use_app, register_date_time')->values([$user, 'STUDENT', 0, date('Y-m-d H:i:s')])->run(), function ($query) {
+            $result = $query->getResult();
+            $result['message'] = Message::SAVED_WITH_SUCCESS;
+            return $result;
+        });
     }
     
-    public function getProfile($user) {
-        $result = selectDB($this->con, 'users', 'id, user, type, can_use_app', 'WHERE user = :user', [':user' => $user]);
-        if ($result != null) {
-            $result = $result[0];
-        } else {
-            $result['user'] = $user;
-            $result['type'] = 'STUDENT';
-            $result['id'] = insertDB($this->con, 'users', 'user, type, can_use_app, register_date, register_time', [$result['user'], $result['type'], 0, date('Y-m-d'), date('H:i:s')])['id'];
-        }
-        $result['message'] = $result != null ? MESSAGE_EXIST : MESSAGE_NOT_EXIST;
-        return $result;
+    public static function getProfile($user) {
+        return Query::helper((new Select(DB::connect(DATABASE_STREET_RACE)))->table('users')->columns('id, user, type, can_use_app')->where('user = :user')->values([':user' => $user])->run(), function ($query) use ($user) {
+            $result = $query->existRows() ? $query->getResult()[0] : ['user' => $user, 'type' => 'STUDENT', 'id' => self::createAccount($user)['id']];
+            $result['message'] = $query->existRows() ? Message::EXIST : Message::NOT_EXIST;
+            return $result;
+        });
     }
     
-    public function canUseApp($user) {
-        $profile = $this->getProfile($user);
-        return $profile['message'] === MESSAGE_EXIST && $profile['can_use_app'];
+    public static function canUseApp($user) {
+        $profile = self::getProfile($user);
+        return $profile['message'] === Message::EXIST && $profile['can_use_app'];
     }
     
 }
