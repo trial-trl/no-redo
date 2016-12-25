da9a8cd61f4465615da5f150f446e5ee01adf154
Matheus Leonardo <mlsm@trialent.com>
2016-12-25 01:47:50 -0300

    feat: add governmental department account support
    - Add Department profile class extending Account\Base
    - Add GOVERNMENTAL_DEPARTMENT account type constant
    - Add governmentalDepartmentAuth() method for department authentication
    - Add Department account creation support in create() method
    - Add Department cookie handling in concludeAuthenticationWeb()
    - Add department cookie constants (COOKIE_TGD_ID_TRIAL, COOKIE_TGD_NAME, COOKIE_TGD_EMAIL)
    - Update FixItAccount to use DB::connect() instead of ConnectDB
    - Replace TRIALAccount:: constant references with self:: in authentication methods

--- php/utils/Constant.php
+++ php/utils/Constant.php
@@ -134,6 +134,11 @@ const COOKIE_TG_ID_TRIAL = 'TRL_TG_idTRIAL';
 const COOKIE_TG_NAME = 'TRL_TG_name';
 const COOKIE_TG_EMAIL = 'TRL_TG_email';
 
+// Department cookies
+const COOKIE_TGD_ID_TRIAL = 'TRL_TGD_idTRIAL';
+const COOKIE_TGD_NAME = 'TRL_TGD_name';
+const COOKIE_TGD_EMAIL = 'TRL_TGD_email';
+
 // Common cookie's
 const COOKIE_TYPE = 'TRL_type';
 
