60b52b4c3b7077172499dca1c086b2f196fbbd51
Matheus Leonardo <mlsm@trialent.com>
2018-11-21 01:22:57 -0300

    Version 0.3.0 - Major changes
    ===============================
    General:
    
    - Library reestructured:
    	- trl-no-redo was descentralized:
    		- Project-specific sub-accounts of TRIAL Account were moved to the respective projects
    		- Map.class.php removed
    	- Filetree changed
    	- Adapted to more generic use-cases
    
    - General syntax/semantic improvements
    
    ===============================
    JavaScript api:
    
    - Added in api.js:
    	Model-View-Controller structure complete support under T.MVC.* package:
    		- Support for data-bind (OneWayView/TwoWayView)
    		- Completely modular (blocks)
    		- Module works on local context, and has access to global context
    		- Utilities for JavaScript of blocks
    		- Dynamic callbacks definition under 'on' prefix
    		- Support for template, CSS, and JavaScript bundle load
    		- Support for template parsering on load
    		- And much more...
    	storageAvailable(): check for available space in local or session storage
    
    - All modules load events:
    	T.Event.loaded
    	T.Account.loaded
    	T.Cookies.loaded
    	T.Design.loaded
    	T.elements.loaded
    	T.elements.TRIAL.Button.loaded
    	T.elements.Editable.loaded
    	T.elements.TRIAL.Loading.loaded
    	T.elements.TRIAL.Logo.loaded
    	T.elements.Pagination.loaded
    	T.elements.Popup.loaded
    	T.elements.TRIAL.SelectableItems-Ready
    	T.elements.TRIAL.Slideshow-Ready
    	T.elements.Suggestions.loaded
    	T.elements.Tabs.loaded
    	T.History.loaded
    	T.Navigation.loaded
    	T.Utils.loaded
    
    ===============================
    PHP:
    
    - Improved Database model
    	Proxy.class.php:
    		- Added:
    			CRUD operations (get, edit, insert, delete)
    			getIdentifier(): return the ID of model
    			fetchDataIntoModel(): array-to-model converter
    	Model.class.php:
    		- Now abstract
    		- Added:
    			parseDate(): string/object to DateTime parser
    			__filterToDatabase(): iterable of each item in model-converted array
    			toDatabase(): convert model to database-values-supported array
    		- Now implements JsonSerializable

---
+++ js/src/elements/trl-suggestions.js
@@ -0,0 +1,118 @@
+/* 
+ * (c) 2017 TRIAL.
+ * Created on 03/09/2016, 13:13:30 at Fix It₢ project.
+ * Moved to T on 10/05/2017, 13:32:26.
+ * Moved to here on 18/06/2017, 21:05:24.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+(function (T) {
+  
+  T.elements.custom(T.elements.TRL_SUGGESTIONS, {
+    prototype: Object.create(HTMLDivElement.prototype, {
+      createdCallback: {
+        value: function () {
+          this.setup();
+        }
+      },
+      attributeChangedCallback: {
+        value: function (attr_name, old_value, new_value) {
+          if (attr_name === 'trl-store-accepted-suggestions') {
+            if (new_value === 'true') {
+              this.open();
+            } else {
+              this.close();
+            }
+          }
+        }
+      },
+      attachedCallback: {
+        value: function () {
+          this.setup();
+        }
+      },
+      accepted: {
+        value: []
+      },
+      opened: {
+        set: function (opened) {
+          this.setAttribute('trl-opened', opened ? 'true' : 'false');
+        },
+        get: function () {
+          return this.getAttribute('trl-opened') === 'true';
+        }
+      },
+      close: {
+        value: function () {
+          if (this.opened)
+            this.opened = false;
+        }
+      },
+      open: {
+        value: function () {
+          if (!this.opened)
+            this.opened = true;
+        }
+      },
+      storeAcceptedSuggestions: {
+        set: function (store) {
+          this.setAttribute('trl-store-accepted-suggestions', store.toString());
+        },
+        get: function () {
+          return this.getAttribute('trl-store-accepted-suggestions') === 'true';
+        }
+      },
+      onacceptsuggestion: {
+        set: function (callback) {
+          this.addEventListener('acceptsuggestion', callback);
+        }
+      },
+      setup: {
+        value: function () {
+          var that = this,
+              observer = new MutationObserver(x);
+
+          observer.observe(this, {
+            childList : true,
+            attributes: true
+          });
+
+          function x() {
+
+            for (var i = 0, total = that.children.length; i < total; i++) {
+
+              var suggestion = that.children[i];
+
+              suggestion.onclick = function (e) {
+                if (that.storeAcceptedSuggestions) {
+                  that.accepted.push(e.target);
+                  that.removeChild(e.target);
+                }
+                that.dispatchEvent(new CustomEvent('acceptsuggestion', { detail: { suggestion: e.target } }));
+                that.close();
+              };
+
+            }
+
+          }
+        }
+      }
+    })
+  }, 'Suggestions');
+  
+  window.dispatchEvent(new CustomEvent('T.elements.Suggestions.loaded'));
+  
+})(window.T);
