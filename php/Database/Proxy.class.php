bc9f3730c3fb349c46e76a225910f24575d9f681
mlsm <mlsm@trialent.com>
2020-02-21 01:56:28 -0300

    - Aprimorado as classes para reduzir a escrita manual do SQL     - Adicionado um parser que lida com todos os dados especiais, construído em cima dos já existentes     - Adicionado um gerador de SQL de retorno de dados das tabelas em formato JSON, evitando a escrita manual e repetitiva     - Adicionado suporte à identificação automática do valor que deve ser inserido no banco de dados     - Adicionado o método getAll no Proxy, que retornará múltiplas linhas, diferente do get     - Adicionado suporte aos aliases e lidar com os aliases automaticamente ao obter nomes de tabelas e colunas
    - Corrigido problema de cor no T.Color
    
    - Melhorado os recursos de select no trl-editable
    
    - Corrigido problema com barras adicionais em URL's no T.Router
    
    - Adicionado o restante da identidade visual em common.css
    
    - Adicionado a possibilidade de remover interceptadores em T.Utils.ajax

--- php/Database/Proxy.class.php
+++ php/Database/Proxy.class.php
@@ -45,6 +45,11 @@ abstract class Proxy implements Proxy\IProxy {
   private $table;
   /**
    *
+   * @var string
+   */
+  private $alias = null;
+  /**
+   *
    * @var array
    */
   private $columns;
@@ -77,19 +82,122 @@ abstract class Proxy implements Proxy\IProxy {
    * 
    * @return string
    */
-  public function getTable(): string {
-    return $this->table;
+  public function getTable(bool $with_alias = false): string {
+    return $this->table . ($with_alias ? ' ' . $this->getAlias(false) : null);
   }
   
+  /**
+   * 
+   * @return array
+   */
   public function getColumns(): array {
     return $this->columns;
   }
-
-  public function get($model) {
+  
+  /**
+   * 
+   * @param bool $append_dot
+   * @return type
+   */
+  protected function getAlias(bool $append_dot = true) {
+    return $this->alias ? ($this->alias . ($append_dot ? '.' : null)) : null;
+  }
+  
+  /**
+   * 
+   * @param array $columns
+   * @param bool $add_alias
+   * @param bool $strict
+   * @return type
+   * @throws \InvalidArgumentException
+   */
+  protected function parseColumns(array $columns = [], bool $add_alias = true, bool $strict = true) {
+    if ($strict === true && !empty($columns)) {
+      $unknown_columns = array_diff(
+                                        array_values($columns), 
+                                        array_values($this->getColumns())
+                         );
+      if (!empty($unknown_columns)) {
+        throw new \InvalidArgumentException('Unknown column \'' . implode(', ', array_keys($unknown_columns)) . '\' for table \'' . $this->getTable() . '\'');
+      }
+    } else {
+      $columns = array_values($this->getColumns());
+    }
+    if ($this->hasAlias() && $add_alias === true) {
+      return preg_replace('/^/', $this->getAlias(), $columns);
+    }
+    return $columns;
+  }
+  
+  /**
+   * 
+   * @param string $column
+   * @return string
+   */
+  protected function col(string $column) {
+    return $this->getAlias() . $column;
+  }
+  
+  /**
+   * 
+   * @param string $alias
+   */
+  protected function addAlias(string $alias = null) {
+    if (!empty($alias)) {
+      if ($alias === 'table') {
+        $this->alias = $this->getTable();
+      } else {
+        $this->alias = $alias;
+      }
+    } else {
+      $table_parts = explode('_', $this->table);
+      $alias = '';
+      foreach ($table_parts as $part) {
+        $alias .= $part[0];
+      }
+      $this->alias = $alias;
+    }
+    if (!empty($alias)) {
+      $this->alias = str_replace('.', '', strtolower($this->alias));
+    } else {
+      $this->removeAlias();
+    }
+  }
+  
+  /**
+   * 
+   */
+  protected function removeAlias() {
+    $this->alias = null;
+  }
+  
+  /**
+   * 
+   * @return bool
+   */
+  protected function hasAlias(): bool {
+    return !empty($this->alias);
+  }
+  
+  /**
+   * 
+   * @param type $model
+   * @param callable $cb
+   * @param array $columns
+   * @return type
+   */
+  public function get($model, callable $cb = null, array $columns = []) {
     $model_id = $this->getIdentifier($model);
     $dsql = $this->getConnection()->dsql();
-    return $this->getConnection()->atomic(function () use ($dsql, $model_id) {
-      $data = $dsql->table($this->getTable())->field(array_values($this->getColumns()))->where(self::ID, $model_id)->getRow();
+    $columns = $this->parseColumns($columns);
+    return $this->getConnection()->atomic(function () use ($dsql, $model_id, $cb, $columns) {
+      $q = $dsql->table($this->getTable(), $this->getAlias(false))
+                ->field($columns)
+                ->where($this->getAlias() . self::ID, $model_id);
+      if (is_callable($cb)) {
+        $cb($q);
+      }
+      $data = $q->getRow();
       if (!empty($data)) {
         return static::fetchDataIntoModel($data);
       }
@@ -97,6 +205,38 @@ abstract class Proxy implements Proxy\IProxy {
     });
   }
 
+  /**
+   * 
+   * @param callable $cb
+   * @param array $columns
+   * @param array $opts
+   * @return array
+   */
+  public function getAll(callable $cb = null, array $columns = [], array $opts = []): array {
+    $dsql = $this->getConnection()->dsql();
+    $columns = $this->parseColumns($columns);
+    return $this->getConnection()->atomic(function () use ($dsql, $cb, $columns, $opts) {
+      $models = [];
+      $q = $dsql->table($this->getTable(), $this->getAlias(false))
+                ->field($columns);
+      static::_applyOptions($q, $opts);
+      if (is_callable($cb)) {
+        $cb($q);
+      }
+      $data = $q->get();
+      foreach ($data as $model) {
+        $models[] = static::fetchDataIntoModel($model);
+      }
+      return $models;
+    });
+  }
+  
+  /**
+   * 
+   * @param type $model
+   * @param array $changes
+   * @return bool
+   */
   public function edit($model, array $changes): bool {
     $model_id = $this->getIdentifier($model);
     $q = $this->getConnection()->dsql()->table($this->getTable());
@@ -104,9 +244,15 @@ abstract class Proxy implements Proxy\IProxy {
       $q->set($k, $v);
     }
     $update = $q->where(self::ID, $model_id)->update();
-    return true;//$update->rowCount() >= 1;
+    return $update->rowCount() > 0;
   }
 
+  /**
+   * 
+   * @param Model $model
+   * @return int
+   * @throws \InvalidArgumentException
+   */
   public function insert($model): int {
     if (!($model instanceof Model) && !is_array($model)) {
       throw new \InvalidArgumentException;
@@ -114,13 +260,7 @@ abstract class Proxy implements Proxy\IProxy {
     $q = $this->getConnection()->dsql()->table($this->getTable());
     return $this->getConnection()->atomic(function () use ($q, $model) {
       $put = $model instanceof Model ? $model->toDatabase() : $model;
-      $unknown_columns = array_diff(
-              array_keys($put), 
-              array_values($this->getColumns())
-      );
-      if (!empty($unknown_columns)) {
-        throw new \InvalidArgumentException('Unknown column \'' . implode(', ', array_keys($unknown_columns)) . '\' for table \'' . $this->getTable() . '\'');
-      }
+      $this->parseColumns($put, false);
       foreach ($put as $c => $v) {
         $q->set($c, $v);
       }
@@ -147,6 +287,35 @@ abstract class Proxy implements Proxy\IProxy {
   
   /**
    * 
+   * @param \atk4\dsql\Query $query
+   * @param array $opts
+   */
+  protected function _applyOptions(\atk4\dsql\Query &$query, array &$opts = []) {
+    static::_parseOptions($opts);
+    if (!empty($opts[Model::QUERY_IGNORE_ID])) {
+      $query->where($this->getAlias() . self::ID, $query->expr('NOT IN ([ids])', ['ids' => implode(', ', $opts[Model::QUERY_IGNORE_ID])]));
+    }
+    $query->limit($opts[Model::QUERY_LIMIT], $opts[Model::QUERY_LIMIT] * $opts[Model::QUERY_OFFSET]);
+  }
+  
+  /**
+   * 
+   * @param array $opts
+   */
+  protected function _parseOptions(array &$opts) {
+    $opts[Model::QUERY_IGNORE_ID] = isset($opts[Model::QUERY_IGNORE_ID]) && (is_int($opts[Model::QUERY_IGNORE_ID]) || is_array($opts[Model::QUERY_IGNORE_ID])) 
+                                    ? $opts[Model::QUERY_IGNORE_ID]
+                                    : null;
+    $opts[Model::QUERY_LIMIT]     = isset($opts[Model::QUERY_LIMIT]) && is_int($opts[Model::QUERY_LIMIT])
+                                    ? $opts[Model::QUERY_LIMIT] 
+                                    : 20;
+    $opts[Model::QUERY_OFFSET]    = isset($opts[Model::QUERY_OFFSET]) && is_int($opts[Model::QUERY_OFFSET]) 
+                                    ? $opts[Model::QUERY_OFFSET] 
+                                    : 0;
+  }
+  
+  /**
+   * 
    * @throws \InvalidArgumentException
    * 
    * Created on 06/06/2018 15:48:08
@@ -158,6 +327,61 @@ abstract class Proxy implements Proxy\IProxy {
       throw new \InvalidArgumentException;
     }
     return (int) ($is_obj ? $model->getId() : $model);
+  }
+  
+  /**
+   * 
+   * @param array $columns              <b>Optional</b>
+   *                                    Columns to generate
+   * @param string $alias              <b>Optional</b>
+   *                                    Alias to add before each column
+   * @return string                     The string Json-formatted
+   * @throws \InvalidArgumentException  If any column isn't declared in proxy class
+   * 
+   * Created on 16/02/2020 11:11:10
+   */
+  public function generateJson(array $columns = [], string $alias = null): string {
+    $columns = $this->parseColumns($columns, false);
+    if (!empty($alias)) {
+      $this->addAlias($alias);
+    }
+    
+    $cols = [];
+    foreach ($columns as $key => $value) {
+      if (is_string($key)) {
+        $column_name = $key;
+      } else {
+        $column_name = $value;
+        $value = null;
+      }
+      $column_query = $this->getAlias() . $column_name;
+      
+      $col = '"' . $column_name . '": \', ';
+      
+      if (empty($value)) {
+        $col .= 'IF(' . $column_query . ' IS NOT NULL, CONCAT(\'"\', ' . $column_query . ', \'"\'), \'null\')';
+      } else {
+        if ($value instanceof self) {
+          $col .= $value->generateJson();
+        } else if (is_string($value) && strpos('CONCAT', $value) === 0) {
+          $col .= $value;
+        } else if (is_string($value)) {
+          $col .= '\'"' . $value . '"\'';
+        } else {
+          $col .= '\'' . $value . '\'';
+        }
+      }
+      
+      $col .= ', \'';
+      
+      $cols[] = $col;
+    }
+    
+    $json  = 'CONCAT(\'{';
+    $json .= implode(', ', $cols);
+    $json .= '}\')';
+    
+    return $json;
   }
   
   /**
