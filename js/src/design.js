296787dcfe01152e6386573bc0317a442c567bac
Matheus Leonardo <mlsm@trialent.com>
2026-02-25 17:26:30 -0300

    fix(design): correct hueToRgb premature return causing invalid HSL to RGB conversion
    Remove early return when normalizing hue (t < 0 || t > 1) to ensure proper channel calculation.

--- js/src/design.js
+++ js/src/design.js
@@ -501,11 +501,9 @@ window.T = ( ( T ) => {
         };
 
         Color.hueToRgb = function(p, q, t) {
+          if (t < 0) t += 1;
+          if (t > 1) t -= 1;
           switch (false) {
-            case !(t < 0):
-              return t += 1;
-            case !(t > 1):
-              return t -= 1;
             case !(t < 1 / 6):
               return p + (q - p) * 6 * t;
             case !(t < 1 / 2):
